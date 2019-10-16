7deeb54268fc101088a25ebdcf583378af5f8bc9
king <1162393020@qq.com>
2019-10-16 16:19:45 +0800

    add ehcache

--- spring-boot-hello/src/main/java/com/neo/HelloApplication.java
+++ spring-boot-hello/src/main/java/com/neo/HelloApplication.java
@@ -2,8 +2,14 @@ package com.neo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableCaching
+@Configuration
+@ComponentScan(basePackages = {"com"})
 public class HelloApplication {
 
 	public static void main(String[] args) {
